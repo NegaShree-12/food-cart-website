ff710bb0ad5c49bdba8b0a3dd472a489db527d4b
negashree <negashree2203@gmail.com>
2024-09-23 11:21:15 +0530

    changed the balence

--- foodcart.js
+++ foodcart.js
@@ -9,14 +9,20 @@ document.querySelector('.book').addEventListener('click', function() {
     paymentContent.style.display = this.classList.contains('active') ? 'none' : 'block';
     blueBottomBox.classList.toggle('hidden');
     catlog.style.visibility = this.classList.contains('active') ? 'visible' : 'hidden';
-
+    
     printItems.style.display = 'flex';
+
+    const textToDisplay = document.querySelector('.text-to-display');
+    textToDisplay.style.display = 'none'; 
 });
 
 document.querySelectorAll('.food-image').forEach(image => {
     image.addEventListener('click', function() {
+        const textToDisplay = document.querySelector('.text-to-display');
         const catlog = document.querySelector('.catlog-hidden');
         const printItems = document.querySelector('.print-items');
+
+        textToDisplay.style.display = 'block'; 
 
         catlog.style.visibility = 'visible';
         catlog.querySelector('.catalog').style.display = 'none';
@@ -33,15 +39,13 @@ document.querySelector('.bx-x').addEventListener('click', function() {
     const printItems = document.querySelector('.print-items');
     const bookButton = document.querySelector('.book');
     const blueBottomBox = document.querySelector('.blue-bottom-box');
+    const textToDisplay = document.querySelector('.text-to-display');
 
     paymentContent.style.display = 'block';
-
     catlog.style.visibility = 'hidden';
-
     bookButton.classList.remove('active');
-
     printItems.style.display = 'flex';
-
-
     blueBottomBox.classList.remove('hidden');
+
+    textToDisplay.style.display = 'none';
 });
